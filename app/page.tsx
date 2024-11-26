cad1ae5d968f9fbc3e133d9ea8e54bfc0725fbe9
Stephan Kulla <git.mail@kulla.me>
2024-11-26 17:35:42 +0100

    refactor: Add own components for slide and heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 'use client'
 
-import { Deck, Slide, Heading as SpectacleHeading } from 'spectacle'
+import {
+  Deck,
+  Slide as SpectacleSlide,
+  Heading as SpectacleHeading,
+} from 'spectacle'
 
 export default function Home() {
   return (
     <main>
       <Deck>
-        <Slide backgroundColor="#ffffff">
-          <SpectacleHeading color="#000000">
-            Hello World Presentation
-          </SpectacleHeading>
+        <Slide>
+          <Heading>Hello World Presentation</Heading>
         </Slide>
       </Deck>
     </main>
   )
+}
+
+function Slide({ children }: { children: React.ReactNode }) {
+  return <SpectacleSlide backgroundColor="#ffffff">{children}</SpectacleSlide>
+}
+
+function Heading({ children }: { children: React.ReactNode }) {
+  return <SpectacleHeading color="#000000">{children}</SpectacleHeading>
 }

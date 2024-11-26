2bb9896cd631f7682ff5bbced3bf27064b2c084a
Stephan Kulla <git.mail@kulla.me>
2024-11-26 20:27:30 +0100

    wip: current work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -3,50 +3,64 @@
 import React from 'react'
 import { Heading } from '@radix-ui/themes'
 
-interface SlideProps {
-  id: string
-  title: string
-  nextSlide?: string
-  content?: React.ReactNode
+enum SlideId {
+  Intro = 'intro',
+  LernpfadKarte = 'lernpfad-karte',
+  Lebensweltbezug = 'lebensweltbezug',
+  VorwissenAktivieren = 'vorwissen-aktivieren',
 }
-
-const slides: SlideProps[] = [
-  {
-    id: 'lebensweltbezug',
-    title: 'Lebensweltbezug herstellen',
-    content: (
-      <>
-        <h1>Hello World</h1>
-      </>
-    ),
-    nextSlide: 'vorwissen-aktivieren',
-  },
-  {
-    id: 'vorwissen-aktivieren',
-    title: 'Vorwissen aktivieren',
-  },
-]
 
 export default function Home() {
-  const [currentSlide, setCurrentSlide] = React.useState<SlideProps | null>(
-    slides[0],
-  )
+  const [currentSlide, setCurrentSlide] = React.useState<SlideId>(SlideId.Intro)
 
   return (
-    <main>
-      <Slide {...currentSlide} />
+    <main className="p-4">
+      <CurrentSlide />
     </main>
   )
+
+  function CurrentSlide() {
+    switch (currentSlide) {
+      case SlideId.Intro:
+        return <IntroSlide />
+      case SlideId.LernpfadKarte:
+        return <LernpfadKarteSlide />
+      case SlideId.Lebensweltbezug:
+        return <LebensweltbezugSlide />
+      case SlideId.VorwissenAktivieren:
+        return <VorwissenAktivierenSlide />
+    }
+  }
+
+  function IntroSlide() {
+    return (
+      <Slide
+        title="Intro"
+        nextSlide={SlideId.LernpfadKarte}
+        setCurrentSlide={setCurrentSlide}
+      />
+    )
+  }
 }
 
-function Slide({ title, content, nextSlide }: SlideProps) {
+function Slide({
+  title,
+  nextSlide,
+  setCurrentSlide,
+}: {
+  title: string
+  nextSlide?: SlideId
+  setCurrentSlide: (slide: SlideId) => void
+}) {
   return (
-    <section>
+    <div className="p-4 max-w-xl mx-auto">
       <Heading>{title}</Heading>
-      {content}
       {nextSlide && (
-        <button onClick={() => console.log('next slide')}>Next</button>
+        <button
+          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md"
+          onClick={() => setCurrentSlide(nextSlide)}
+        ></button>
       )}
-    </section>
+    </div>
   )
 }
